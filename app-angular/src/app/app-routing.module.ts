2e8e40f6fdef764ed3a0914f7bfbbaa5ceef401e
RichardH <ricardo.lopezh@softtek.com>
2020-05-10 13:22:05 -0500

    - Add angular sketchpad

--- app-angular/src/app/app-routing.module.ts
+++ app-angular/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SketchpadComponent } from './components/sketchpad/sketchpad.component';
+import { AppComponent } from './app.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'sketchpad', component: SketchpadComponent
+    
+},
+{
+  path: '', component: AppComponent
+},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
